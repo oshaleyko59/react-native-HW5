a56f0672cf00895c303a1dcc8571812be4b92a80
Olga Dmytrenko <114358209+oshaleyko59@users.noreply.github.com>
2023-07-18 13:11:23 -0500

    Update location.js

--- src/utils/location.js
+++ src/utils/location.js
@@ -1,10 +1,1 @@
-import { COLORS } from "../common/constants";
-const GOOGLE_API_KEY = "AIzaSyArxRmovioh9GVM-Vqk3o9YzSuBHrr-RMk"; //TODO: make ENV!!! should not go out
 
-export default function getMapPreview(lat, lng) {
-	//TODO: ??? &signature=YOUR_SIGNATURE`
-	const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
-
-	console.log("mapPreviewUrl>>", imagePreviewUrl);
-	return imagePreviewUrl;
-}
